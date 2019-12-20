71756816d01c5f94f8f0aa7c8f41eacc8b6d834c
Lex <50260829+4gname@users.noreply.github.com>
2019-12-20 21:34:33 +0700

    Fix 12 steps NC2

--- src/main/java/gregtech/api/metatileentity/implementations/GT_MetaTileEntity_BasicBatteryBuffer.java
+++ src/main/java/gregtech/api/metatileentity/implementations/GT_MetaTileEntity_BasicBatteryBuffer.java
@@ -321,19 +321,12 @@ public class GT_MetaTileEntity_BasicBatteryBuffer extends GT_MetaTileEntity_Tier
         return new String[]{
                 getLocalName(),
                 "Stored Items:",
-                (getBaseMetaTileEntity().getAverageElectricInput() > getBaseMetaTileEntity().getAverageElectricOutput()
-                        ? EnumChatFormatting.GREEN+GT_Utility.formatNumbers(mStored) +EnumChatFormatting.RESET+ " EU"
-                        : EnumChatFormatting.RED+GT_Utility.formatNumbers(mStored) +EnumChatFormatting.RESET+ " EU"),
-                //EnumChatFormatting.GREEN+GT_Utility.formatNumbers(mStored) +EnumChatFormatting.RESET+ " EU",
+                (getBaseMetaTileEntity().getAverageElectricInput() > getBaseMetaTileEntity().getAverageElectricOutput() ? EnumChatFormatting.GREEN+GT_Utility.formatNumbers(mStored) +EnumChatFormatting.RESET+ " EU" : EnumChatFormatting.RED+GT_Utility.formatNumbers(mStored) +EnumChatFormatting.RESET+ " EU"),
                 EnumChatFormatting.YELLOW+GT_Utility.formatNumbers(mMax) +EnumChatFormatting.RESET+ " EU",
                 "Average Input:",
                 EnumChatFormatting.GREEN+GT_Utility.formatNumbers(getBaseMetaTileEntity().getAverageElectricInput())+EnumChatFormatting.RESET+" EU/t",
                 "Average Output:",
                 EnumChatFormatting.RED+GT_Utility.formatNumbers(getBaseMetaTileEntity().getAverageElectricOutput())+EnumChatFormatting.RESET+" EU/t"};
-
-
-
-
     }
 
     @Override
