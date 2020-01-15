7d0725646c370c5e1582797621c5d67f9b5220fe
Lex <domian844@gmail.com>
2020-01-15 14:46:57 +0700

    add GT_MetaGenerated_Item_04

---
+++ src/main/java/gregtech/common/items/GT_MetaGenerated_Item_04.java
@@ -0,0 +1,27 @@
+package gregtech.common.items;
+
+import gregtech.api.GregTech_API;
+import gregtech.api.enums.*;
+import gregtech.api.interfaces.ITexture;
+import gregtech.api.items.GT_MetaGenerated_Item_X32;
+import gregtech.api.objects.GT_MultiTexture;
+import gregtech.api.objects.GT_RenderedTexture;
+import gregtech.common.covers.GT_Cover_FluidRegulator;
+
+public class GT_MetaGenerated_Item_04
+        extends GT_MetaGenerated_Item_X32 {
+    public static GT_MetaGenerated_Item_04 INSTANCE;
+
+    public GT_MetaGenerated_Item_04() {
+        super("metaitem.04", new OrePrefixes[]{OrePrefixes.crateGtDust, OrePrefixes.crateGtIngot, OrePrefixes.crateGtGem, OrePrefixes.crateGtPlate});
+        INSTANCE = this;
+        int tLastID = 0;
+        Object[] o = new Object[0];
+    }
+
+
+
+    public boolean doesShowInCreative(OrePrefixes aPrefix, Materials aMaterial, boolean aDoShowAllItems) {
+        return aDoShowAllItems;
+    }
+}
